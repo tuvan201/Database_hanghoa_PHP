ad1eecdbd26a34c806d645322f5da843d899a33b
tuvan201 <97530402+tuvan201@users.noreply.github.com>
2022-03-18 09:39:37 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php 
 include"import.php";
-$sql_query="SELECT * FROM hanghoa";
-$result=mysqli_query($conn,$sql_query);
-//kiểm tra dữ liệu
+$tenhang="";
 ?>
 <a href="insert_Item.php">THÊM</a>
+<a href=""><br>HIỂN THỊ TẤT CẢ</a>
+<?php
+if(isset($_POST["submit_sreach"]) and !empty($_POST["sreach_item"]))
+{
+$tenhang=$_POST["sreach_item"];
+$sql_query="SELECT * FROM hanghoa WHERE Tenhang='$tenhang'";
+$result=mysqli_query($conn,$sql_query);
+}
+else{
+    $sql_query="SELECT * FROM hanghoa";
+    $result=mysqli_query($conn,$sql_query);
+}?>
 <table border="1px" cellpadding="10" cellspacing="0">
     <caption><h2>Bảng hàng hóa</h2></caption>
+    <tr>
+        <form action="" method="post">
+        <td colspan="2">Tìm kiếm:</td>
+        <th colspan="3"><input type="text" name="sreach_item" id="" placeholder="nhập tên hàng cần tìm" value="<?php echo $tenhang; ?>"></th>
+        <th colspan="2"><input type="submit" name="submit_sreach" value="Tìm kiếm"></th>
+        </form>
+    </tr>
     <tr>
         <th>ID</th>
         <th>Tên Hàng</th>
@@ -16,7 +33,7 @@ $result=mysqli_query($conn,$sql_query);
         <th>Hình Ảnh</th>
         <th>Xóa hàng</th>
     </tr>
-<?php
+    <?php
 if(mysqli_num_rows($result)>0)
 {
     while($row=mysqli_fetch_array($result))//trả về 1 dòng của kết quả trả về từ biến $result bằng dạng mảng
@@ -42,11 +59,12 @@ if(mysqli_num_rows($result)>0)
         </form></td>
         </tr>
     <?php }
-}
-else
-{
-    echo "Bảng hàng hóa hiện chưa có dữ liệu";
-}
+    }
+    elseif(mysqli_num_rows($result)==0 and !empty($tenhang))
+    {echo "Không có mặt hàng mà bạn đang tìm kiếm";}
+    else
+    {echo "Bảng hàng hóa hiện chưa có dữ liệu";}
+
 mysqli_close($conn);
 // echo "<pre>";
 // var_dump($row);
